20f186a88753cfd74b0d8b0a590813f79e230ca5
Utsav Sethi <utsavsethi@gmail.com>
2019-03-23 15:24:59 -0400

    Some cleanup/reducing C types to logical types

--- allocator2.c
+++ allocator2.c
@@ -2,24 +2,28 @@
 #include <stdlib.h>
 #include <limits.h>
 
-// Global variable is used instead of define, which allows value to be used in annotations 
-// All sizes are in 16-byte chunks
+// Using global value (instead of #define) allows value to be used in logical annotations 
+// Sizes are in 16-byte chunks for simplicity (so true size is HEAP_SIZE * 16 bytes)
 const int HEAP_SIZE = 256;
 
 typedef struct {
   unsigned int info;
-  void * empty; // Used to align header to 16 bytes
+  void * empty; // Unused, pads header_t to size of 16 bytes
 } header_t;
 
 header_t * heap_start = NULL;
 
-/*@ logic integer _get_size(header_t * h) =
-	h->info >> 4;	
+/*@ lemma bit_shift_bounded:
+		\forall unsigned int x; (0 <= x < UINT_MAX/16) ==>
+			(((x << 4) >> 4) == x);
+*/
+
+/*@ logic integer _get_size(unsigned int x) =
+		(x >> 4);	
 */
 
 /*@ requires \valid(h);
-	ensures \result == _get_size(h);
-	ensures \valid(h);
+	ensures \result == _get_size(h->info);
 	assigns \nothing;
 */
 unsigned int get_size(header_t * h) {
@@ -28,7 +32,7 @@ unsigned int get_size(header_t * h) {
 
 
 /*@ predicate _lsb_set(unsigned int i) = 
-	(unsigned int) (i & ((unsigned int) 1)) > (unsigned int) 0;		
+	(i & 1) == 1;		
 */
 
 /*@ lemma _lsb_not_set:
@@ -47,8 +51,10 @@ unsigned int get_size(header_t * h) {
         \forall unsigned int x; (x >> 32) ==  0;
 */
 
+
+
 /*@ predicate _terminating(header_t * h) = 
-		_lsb_set(h->info) && !_get_size(h);
+		_lsb_set(h->info) && !_get_size(h->info);
 */
 
 /*@
@@ -56,20 +62,20 @@ unsigned int get_size(header_t * h) {
 	case start_reachable{L}:
 		\forall header_t* start; reachable(start,start);
 	case next_reachable{L}:
-		\forall header_t* start, *h; reachable((header_t *) (start + 1 + _get_size(start)), h) ==>
+		\forall header_t* start, *h; reachable((header_t *) (start + 1 + _get_size(start->info)), h) ==>
 		reachable(start,h);
 }
 */
 
 /*@	lemma reachable_trans:
 	\forall header_t *start, *h;
-	reachable(start, h) && \valid(h) ==> reachable(start, (h + 1 + _get_size(h)));	
+	reachable(start, h) && \valid(h) ==> reachable(start, (h + 1 + _get_size(h->info)));	
 */ 
 
 /*@	lemma reachable_size_gt:
 	\forall header_t *start, *h;
 	reachable(start, h) && \valid(h) ==> 
-		_get_size(h) > 0 || _terminating(h);	
+		_get_size(h->info) > 0 || _terminating(h);	
 */ 
 
 /*@ predicate finite{L}(header_t * start) = 
@@ -82,12 +88,12 @@ unsigned int get_size(header_t * h) {
 */
 
 /*@ lemma size_gt:
-	\forall header_t * h; \forall unsigned int size; (_get_size(h) > size) ==>
+	\forall header_t * h; \forall unsigned int size; (_get_size(h->info) > size) ==>
 		\valid(h+ (0..(size+1)));
 */
 
 /*@ lemma non_terminating_block:
-		\forall header_t *start, *h; (reachable(start, h) && !_terminating(h)) ==> \valid(h + 1 + _get_size(h));
+		\forall header_t *start, *h; (reachable(start, h) && !_terminating(h)) ==> \valid(h + 1 + _get_size(h->info));
 */
 
 /*@ requires \valid(h);
@@ -109,8 +115,6 @@ unsigned int is_allocated(header_t * h) {
 
 /*@ requires \valid(h);
 	ensures _lsb_set(h->info);
-	ensures \valid(h);
-	ensures h == \old(h);
 	assigns h->info;
 */
 void mark_allocated(header_t * h) {
@@ -127,38 +131,34 @@ void mark_free(header_t * h) {
 }
 
 /*@ requires \valid(h);
-	requires size <= UINT_MAX/16;
-	ensures _get_size(h) == size;
+	requires 0 < size < UINT_MAX/16;
+	ensures (unsigned int) (h->info >> 4) == size;
 	ensures \valid(h);
 	ensures h == \old(h);
 */
 void mark_size(header_t * h, unsigned int size) {
-	h->info = ((h->info << 28) >> 28) | (size << 4);
+	h->info = (size << 4);
+	//h->info = ((h->info << 28) >> 28) | (size << 4);
 }
-
-/*@ lemma bit_shift_bounded:
-		\forall unsigned int x; 0 <= x < UINT_MAX/16 ==>
-			(x << 4) >> 4 == x;
-*/
 
 /*@ 
 	requires size > 0;
-	requires size < _get_size(h) <= UINT_MAX/16; 
+	requires size < _get_size(h->info) <= UINT_MAX/16; 
 	requires \valid(h+ (0..(size+1)));
 	assigns (h + 1 + size)->info; 
 	assigns h->info;
 	ensures _lsb_set(h->info);
-	ensures _get_size(h) == size;
-	ensures (_get_size(\old(h)) - (size + 1)) == _get_size((h + 1 + size));
+	ensures _get_size(h->info) == size;
+	ensures (_get_size(\old(h->info)) - (size + 1)) == _get_size((h + 1 + size)->info);
 	ensures \valid(h + 1 + size);
 	ensures \result == (header_t *) h + 1;
 	ensures \valid(\result);
 */
 header_t * split_block(header_t * h, unsigned int size) {
-	//unsigned int next_size = get_size(h) - (size + 1);
-	//(h + 1 + size)->info = next_size;
+	unsigned int next_size = get_size(h) - (size + 1);
+	(h + 1 + size)->info = next_size;
 	h->info = (size << 4);
-	//mark_allocated(h);
+	mark_allocated(h);
 	return h+1;
 }
 
@@ -233,7 +233,7 @@ void print_debug(header_t * start) {
 		//printf("Block size: %lu\n", get_size(h));
 		//printf("Allocated: %d\n\n", is_allocated(h));
 		i++;
-		h += (1 + (get_size(h)/16));
+		h += (1 + get_size(h));
 	}	
 	// print terminating block
 	//printf("Block %d at location %p\n", i, h);
